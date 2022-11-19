1765bf42c7ef14a6fcbc336a5c47caf09fcfb8ed
Jorge <jorge6alberto@gmail.com>
2022-11-19 11:52:14 -0600

    Big bang

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame obj = new JFrame();
+        obj.setTitle("Proyecto final de Automatas");
+        obj.setResizable(false);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.getContentPane().setPreferredSize(new Dimension(1600, 1000));
+
+        Automata automata = new Automata();
+        obj.add(automata);
+
+        obj.pack();
+        obj.setLocationRelativeTo(null);
+        obj.setVisible(true);
+
+    }
+
+
 }
